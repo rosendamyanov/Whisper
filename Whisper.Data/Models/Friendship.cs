7abe3b2d4e00763f686bbaaebd7adc94e1ccec67
rosendamyanov <rosen.damyanov7@gmail.com>
2025-11-14 14:21:17 +0200

    Added models

--- Whisper.Data/Models/Friendship.cs
+++ Whisper.Data/Models/Friendship.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Whisper.Data.Models
+﻿namespace Whisper.Data.Models
 {
     public class Friendship
     {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public Guid FriendId { get; set; }
+        public User Friend { get; set; }
+
+        public bool IsAccepted { get; set; } = false;
+
+        public bool IsBlocked { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? AcceptedAt { get; set; }
     }
 }
--- Whisper.Data/Models/Stream.cs
+++ Whisper.Data/Models/Stream.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Whisper.Data.Models
+﻿namespace Whisper.Data.Models
 {
-    internal class Stream
+    public class Stream
     {
+        public Guid Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public bool IsLive { get; set; } = true;
+
+        public DateTime StartedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? EndedAt { get; set; }
+
+        public Guid HostUserId { get; set; }
+        public User HostUser { get; set; }
+
+        public ICollection<User> Viewers { get; set; } = new List<User>();
+
+        public int TotalViews { get; set; } = 0;
+
+        public string? SessionId { get; set; }
     }
 }
