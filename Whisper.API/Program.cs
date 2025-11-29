1803136495c2cf91f49ae0f01c07a2d8f2e92f1c
rosendamyanov <rosen.damyanov7@gmail.com>
2025-11-29 03:14:31 +0200

    fix(middleware): fixed JWT authorization

--- Whisper.API/Program.cs
+++ Whisper.API/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
+using System.Text;
 using Whisper.Authentication.Configuration;
 using Whisper.Authentication.Data;
 using Whisper.Authentication.Data.Interfaces;
@@ -15,8 +18,10 @@ using Whisper.Data.Context;
 using Whisper.Data.Repositories;
 using Whisper.Data.Repositories.Interfaces;
 using Whisper.Services;
+using Whisper.Services.Factories;
 using Whisper.Services.Factories.ChatFactory;
 using Whisper.Services.Factories.Interfaces;
+using Whisper.Services.Services;
 using Whisper.Services.Services.Interfaces;
 
 namespace Whisper.Api
@@ -34,20 +39,53 @@ namespace Whisper.Api
             builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
             builder.Services.AddHttpContextAccessor();
 
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                    ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Secret"]!))
+                };
+
+                // Read token from cookie
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        context.Token = context.Request.Cookies["AccessToken"];
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
             // Add services to the container.
             // Repositories
             builder.Services.AddScoped<IAuthRepository, AuthRepository>();
             builder.Services.AddScoped<IChatRepository, ChatRepository>();
+            builder.Services.AddScoped<IFriendshipRepository, FriendshipRepository>();
 
 
             // Services
             builder.Services.AddScoped<IAuthService, AuthService>();
             builder.Services.AddScoped<ITokenService, TokenService>();
             builder.Services.AddScoped<IChatService, ChatService>();
+            builder.Services.AddScoped<IFriendshipService, FriendshipService>();
 
             // Factories
             builder.Services.AddScoped<IAuthFactory, AuthFactory>();
             builder.Services.AddScoped<IChatFactory, ChatFactory>();
+            builder.Services.AddScoped<IFriendshipFactory, FriendshipFactory>();
 
             // Validators
             builder.Services.AddScoped<IEmailValidation, EmailValidation>();
@@ -89,7 +127,8 @@ namespace Whisper.Api
             }
 
             app.UseHttpsRedirection();
-
+            app.UseRouting();
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
